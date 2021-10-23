8d7a57c192ccb9ecc2cd07787a68724f5ae15d62
Agus Eko Minarno <36905991+agusekominarno@users.noreply.github.com>
2021-10-23 16:03:33 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 import matplotlib
-import numpy as np
 from skimage.feature import greycomatrix, greycoprops
 from sklearn.metrics.cluster import entropy
+import numpy as np
+import cv2
+import csv
+import math
+import scipy.spatial.distance as dist
+from PIL import Image
+import skimage.io
