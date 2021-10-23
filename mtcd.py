fb486062ee3a0986ac49747b7d6a8f78f8200eed
Agus Eko Minarno <36905991+agusekominarno@users.noreply.github.com>
2021-10-23 20:57:48 +0700

    Update mtcd.py

--- mtcd.py
+++ mtcd.py
@@ -6,7 +6,7 @@ from matplotlib.pylab import *
 from skimage.feature import greycomatrix, greycoprops
 from sklearn.metrics.cluster import entropy
 import numpy as np
-#import cv2
+import cv2
 import csv
 import math
 import scipy.spatial.distance as dist
