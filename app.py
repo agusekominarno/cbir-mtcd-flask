a1b678a373a0d2524d2c77ed53e42787a8bd8ca0
agusekominarno <agoes.minarno@gmail.com>
2021-10-26 15:19:41 +0700

    query by sampe image
    Masih ada error di query kedua dan seterusnya

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import os
 from flask import Flask, flash, request, redirect, url_for, send_from_directory, Request, render_template
 from werkzeug.utils import secure_filename
 import mtcd
+from PIL import Image
+
+datahasil = os.listdir('static/result/')
 
 UPLOAD_FOLDER = 'static/uploads/'
 RESULT_FOLDER = 'static/result/'
@@ -41,6 +44,41 @@ def upload_file():
 def result_file():
     return render_template('index.html')
 
+@app.route('/predict', methods=['GET', 'POST'])
+def mtcd_predict():
+    if request.method == 'POST':
+        filename = request.form.get('input_image')
+        if filename and allowed_file(filename):  
+            img = Image.open(filename)
+            # img = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB)
+            img = img.save("static/uploads/queryImg.jpg")             
+            data = os.path.join('static/uploads/queryImg.jpg')
+            mtcd.process_file(data)
+            return redirect('/')
+    return render_template('/index.html', len = len(datahasil), datahasil = datahasil)
+
+@app.route('/test', methods=['GET', 'POST'])
+def mtcd_upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename("queryImg.jpg")
+            data = os.path.join('static/uploads/', filename)
+            file.save(data)
+            data = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            mtcd.process_file(data)
+            return redirect('/')
+    return render_template('/index.html', len = len(datahasil), datahasil = datahasil)
+
 @app.after_request
 def add_header(r):
     
@@ -53,5 +91,6 @@ def add_header(r):
     r.headers['Cache-Control'] = 'public, max-age=0'
     return r
 
+#please place this code at bottom
 if __name__ == '__main__':
     app.run()
